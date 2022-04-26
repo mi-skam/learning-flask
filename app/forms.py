e8ccb3966cc77edc465db2721fc0beaa33b6c383
mi-skam <maksim.codes@mailbox.org>
2022-04-26 15:21:51 +0200

    3 - Web Forms

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from tokenize import String
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
